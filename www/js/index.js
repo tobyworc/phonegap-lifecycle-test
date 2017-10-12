e9224b3da0e592357d815feef7767cfaf83defae
tobyworc <32363091+tobyworc@users.noreply.github.com>
2017-10-12 14:04:17 +0100

    Commented

--- www/js/index.js
+++ www/js/index.js
@@ -1,44 +1,44 @@
-var paused_count =0;
-var resumed_count = 0;
-var launched_count = 0;
+var paused_count =0;            //set paused_count to 0
+var resumed_count = 0;          //set resumed_count to 0
+var launched_count = 0;         //set launched_count to 0
 
-document.addEventListener("deviceready", onDeviceReady, false);
+document.addEventListener("deviceready", onDeviceReady, false); //creates event listener for when the device is ready. 
 		
 	
 function updateDisplay() {
-	$("#launched").text("Application launched: " + launched_count);
-	$("#resumed").text("Application paused: " + paused_count);
-	$("#paused").text("Application resumed: " + resumed_count);
+	$("#launched").text("Application launched: " + launched_count);    //displayed count for number of times application is launched
+	$("#resumed").text("Application paused: " + paused_count);         //displayed count for number of times application is paused
+	$("#paused").text("Application resumed: " + resumed_count);        //displayed count for number of times application is resumed
 }
 
 
 // device APIs are available
 //
-    function onDeviceReady() {
+    function onDeviceReady() {                                      //run this function when called, called when device is ready 
 	
-	document.addEventListener("resume", onResume, false);
-	document.addEventListener("pause", onPause, false);
+	document.addEventListener("resume", onResume, false);      ////creates event listener for when the app has been resumed. 
+	document.addEventListener("pause", onPause, false);        ////creates event listener for when the app has been paused. 
 	
-	launched_count++;
-	updateDisplay();
+	launched_count++;      //add one to count for number of times application has been launched
+	updateDisplay();       //run update display function
 	    
-	alert("device ready");
+	alert("device ready");     //pop up alert saying device ready
     }
 
 
-    function onPause() {
+    function onPause() {         //run this function when called, called when app is paused 
 	
-	paused_count++;
-	updateDisplay();
+	paused_count++;            //add one to count for number of times application has been paused
+	updateDisplay();           //run update display function
 	    
-	alert("pause");
+	alert("pause");            //pop up alert saying device ready
     }
 	
 
-    function onResume() {
+    function onResume() {       //run this function when called, called when app is resumed 
 		
-	resumed_count++;
-	updateDisplay();
+	resumed_count++;           //add one to count for number of times application has been resumed
+	updateDisplay();           //run update display function
 	    
-	alert("resume");
+	alert("resume");           //pop up alert saying device ready
     }
